2f09f266714b27f0f6948bd16e79f2b0cddd8252
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2018-09-08 23:22:01 +0300

    Create story.

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,22 @@
+class StoriesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @story = Story.create(story_params)
+
+    if @story.save
+      redirect_to root_path, alert: 'Story created succesfully'
+    else
+      redirect_to root_path, alert: 'Could not save story'
+    end
+  end
+
+  private
+    def story_params
+      params.require(:story).permit(:title, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'stories/index'
+  get 'stories/new'
+  post 'stories/create'
+  root 'home#index'
 end
